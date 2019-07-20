f35a89b42e7064fd06b1c9d47a59ebbcfb2d8467
Leonardo Barros <nickolback@gmail.com>
2019-07-20 06:57:49 -0300

    Comments

---
+++ exercise-js/Fundamentals/comments.js
@@ -0,0 +1,17 @@
+//Using comment in the code.
+
+// One line
+console.log("Comments in the code")
+
+/*
+  Comments with
+  multiples lines.
+*/
+console.log("My special code")
+
+/*
+*
+* Other sytle :(
+*
+*/
+console.log("Other style of comment.")
