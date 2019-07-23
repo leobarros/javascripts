a4a2288b62dd41b8ffd96014e02ed48e04b839f3
Leonardo Barros <nickolback@gmail.com>
2019-07-23 20:55:40 -0300

    Examples strings and numbem in html

---
+++ tests/scripts/strings-numbers.js
@@ -0,0 +1,8 @@
+var editH1 = document.querySelector('h1')
+editH1.textContent = 'My h1 html tag'
+
+var editP = document.querySelector('.text')
+editP.textContent = 'My p html tag'
+
+var editNumber = document.querySelector('.number')
+editNumber.textContent = "One, two, three, four, five".split(',')
