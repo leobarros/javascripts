d5d5e932ed395ed265ce6db1d97dc2ee883a2a2f
Leonardo Sales <lsales@corp.globo.com>
2019-08-18 10:36:21 -0300

    Create a object

---
+++ exercise-js/Fundamentals/objects.js
@@ -0,0 +1,9 @@
+const prod1 = {};
+prod1.name = 'Iphone XS';
+prod1.price = 9999.99;
+prod1.capacity = 512;
+prod1.display = 6.5;
+prod1.chip = 'A12';
+
+console.log('Product: ' + `${prod1.name}` + ' Price: '+ `${prod1.price}` +
+            ' display: ' + `${prod1.display}` + ' Chip: ' + `${prod1.chip}`);
