7720c3527a368a1a22b799d9199ca071a148871c
Leonardo Sales <lsales@corp.globo.com>
2019-08-20 20:12:49 -0300

    Using null and undefined

---
+++ exercise-js/Fundamentals/null_undefined.js
@@ -0,0 +1,13 @@
+let value
+
+console.log(value);
+
+value = null;
+console.log(value);
+
+const product = {}
+console.log(product.price);
+console.log(product);
+product.price = 3.50;
+console.log(product);
+product.price = null;
