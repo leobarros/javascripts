a69fb8cd32356b8f20563cdf5024064c1f44cb43
Leonardo Sales <lsales@corp.globo.com>
2019-08-20 19:34:01 -0300

    New way to array output

--- exercise-js/Fundamentals/arrays_copy.js
+++ exercise-js/Fundamentals/arrays_copy.js
@@ -3,7 +3,13 @@ let dogs = [];
 let cats = [];
 
 dogs.push(animals[0], animals[2], animals[4], animals[5]);
-console.log('Only dogs: ' + `${dogs}`);
-
 cats.push(animals[1], animals[3]);
-console.log('Only cats: ' + `${cats}`);
+
+console.log('Only Dogs: ')
+for (let i = 0; i < dogs.length; i ++) {
+  console.log(dogs[i]);
+}
+console.log('\n' + 'Only cats: ')
+for (let j = 0; j < cats.length; j++) {
+  console.log(cats[j]);
+}
