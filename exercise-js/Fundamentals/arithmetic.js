08e8e2ce5f2786501b8bb99df7c8bd9e194e0246
Leonardo Barros <nickolback@gmail.com>
2021-03-04 11:26:29 -0300

    arithmetic

---
+++ exercise-js/Fundamentals/arithmetic.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const sum = a + b + c + d
+const substraction = d -b
+const multiplication = a * b
+const division = d / a
+const mod = a % 2
+
+console.log(sum, substraction, multiplication, division, mod)
